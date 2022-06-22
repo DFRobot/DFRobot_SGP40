5c3ef5b8f6fcc27e848131e126be265c23b89b0d
fary <fary_young@outlook.com>
2022-06-22 09:10:38 +0800

    Translation of the demo

--- examples/getVocIndex/getVocIndex.ino
+++ examples/getVocIndex/getVocIndex.ino
@@ -15,12 +15,12 @@
 #include <DFRobot_SGP40.h>
 
 /* 
- * 方法一：传入指定的I2C对象地址
+ * Method 1: Pass in the address of the specified I2C object
  * #include <Wire.h>
  * DFRobot_SGP40    mySgp40(&Wire);
  
- * 方法二：使用默认的I2C对象&Wire
- * I2C 默认地址：0x59
+ * Method 2: Use the default I2C object &Wire
+ *I2C Default address: 0x59
  */
 
 //#include <Wire.h>
@@ -32,8 +32,8 @@ void setup() {
   Serial.begin(115200);
   Serial.println("sgp40 is starting, the reading can be taken after 10 seconds...");
   /*
-   * 传感器预热时间为10s。
-   * duration:初始化等待时间。单位：毫秒。建议duration>=10000ms
+   * The preheating time of the sensor is 10s.
+   * duration:Initialize the wait time. Unit: millisecond. Suggestion: duration > = 10000 ms
    */
   while(mySgp40.begin(/*duration = */10000) !=true){
     Serial.println("failed to init chip, please check if the chip connection is fine");
@@ -42,25 +42,24 @@ void setup() {
   Serial.println("----------------------------------------------");
   Serial.println("sgp40 initialized successfully!");
   Serial.println("----------------------------------------------");
-  /* 
-   * 设置当前环境中的相对湿度和温度
-   * 传感器内部已进行温湿度校准，若需要得到更精确的voc指数，请打开注释
-   * relativeHumidity：环境相对湿度，指空气中的水汽含量。范围：0-100，单位：%RH，例如：50%
-   * temperature：环境温度。范围：-10~50，单位：°C，例如：20°C
+  /**
+   * @brief  Set the temperature and humidity
+   * @param  relativeHumidityRH  Current environmental relative humidity value, range 0-100, unit: %RH
+   * @param  temperatureC  Current ambient temperature, range -10~50, unit: °C
    */
   //mySgp40.setRhT(/*relativeHumidity = */ 50, /*temperature = */ 20);
   
 }
 
 void loop() {
-  /* 
-   * 获取voc指数
-   * voc指数可直接指示空气质量的好坏。数值越大，空气质量越差
-   *    0-100，no need to ventilate,purify
-   *    100-200，no need to ventilate,purify
-   *    200-400，ventilate,purify
-   *    400-500，ventilate,purify intensely
-   * 返回voc指数，范围：0-500
+  /**
+   * @brief  Measure VOC index after humidity compensation
+   * @note   VOC index can indicate the quality of the air directly. The larger the value, the worse the air quality.
+   * @note       0-100，no need to ventilate, purify
+   * @note       100-200，no need to ventilate, purify
+   * @note       200-400，ventilate, purify
+   * @note       400-500，ventilate, purify intensely
+   * @return The VOC index measured, ranged from 0 to 500
    */
   uint16_t index = mySgp40.getVoclndex();
   
